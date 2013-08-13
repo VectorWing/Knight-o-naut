6877c393f8cf3c7992c8a882935d0679da7ce893
Rachel J. Morris <racheljmorris@gmail.com>
2013-08-13 15:22:41 -0500

    Temporary level generation

--- main.cpp
+++ main.cpp
@@ -4,16 +4,34 @@
 #include <SFML/Window.hpp>
 
 #include "Borka/Application.h"
+#include "Borka/Level.h"
 
 int main()
 {
-    borka::Application application( "Knight-o-naut", sf::Vector2i( 640, 480 ) );
+    borka::Application application( "Knight-o-naut", sf::Vector2i( 1280, 720 ) );
+
+	// Temporary
+	sf::Texture tileset;
+	if ( !tileset.loadFromFile( "assets/graphics/tileset.png" ) )
+	{
+		std::cerr << "Error loading tileset" << std::endl;
+	}
+
+	sf::Texture player;
+	if ( !player.loadFromFile( "assets/graphics/knight.png" ) )
+	{
+		std::cerr << "Error loading knight" << std::endl;
+	}
+
+	borka::Level level;
+	level.Setup( tileset );
 
     while ( !application.IsDone() )
     {
 		application.Update();
 
 		application.BeginDraw();
+		level.Draw( application.GetWindow() );
 		application.EndDraw();
     }
 
