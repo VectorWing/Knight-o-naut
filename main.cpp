ff927dbb572ab30d47a2527d0dada07a6c60113b
Rachel J. Morris <racheljmorris@gmail.com>
2013-08-13 15:37:48 -0500

    Player movement (WIP)

--- main.cpp
+++ main.cpp
@@ -11,27 +11,54 @@ int main()
     borka::Application application( "Knight-o-naut", sf::Vector2i( 1280, 720 ) );
 
 	// Temporary
-	sf::Texture tileset;
-	if ( !tileset.loadFromFile( "assets/graphics/tileset.png" ) )
+	sf::Texture imgTileset;
+	if ( !imgTileset.loadFromFile( "assets/graphics/tileset.png" ) )
 	{
 		std::cerr << "Error loading tileset" << std::endl;
 	}
 
-	sf::Texture player;
-	if ( !player.loadFromFile( "assets/graphics/knight.png" ) )
+	sf::Texture imgPlayer;
+	if ( !imgPlayer.loadFromFile( "assets/graphics/knight.png" ) )
 	{
 		std::cerr << "Error loading knight" << std::endl;
 	}
+	sf::Sprite player;
+	player.setTexture( imgPlayer );
+	player.setPosition( 320, 240 );
 
 	borka::Level level;
-	level.Setup( tileset );
+	level.Setup( imgTileset );
 
     while ( !application.IsDone() )
     {
 		application.Update();
 
+		if ( sf::Keyboard::isKeyPressed( sf::Keyboard::Escape ) )
+		{
+			level.Setup( imgTileset );
+		}
+
+		if ( sf::Keyboard::isKeyPressed( sf::Keyboard::Left ) )
+		{
+			player.move( -5, 0 );
+		}
+		else if ( sf::Keyboard::isKeyPressed( sf::Keyboard::Right ) )
+		{
+			player.move( 5, 0 );
+		}
+
+		if ( sf::Keyboard::isKeyPressed( sf::Keyboard::Up ) )
+		{
+			player.move( 0, -5 );
+		}
+		else if ( sf::Keyboard::isKeyPressed( sf::Keyboard::Down ) )
+		{
+			player.move( 0, 5 );
+		}
+
 		application.BeginDraw();
 		level.Draw( application.GetWindow() );
+		application.GetWindow().draw( player );
 		application.EndDraw();
     }
 
